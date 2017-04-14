1436d7a85ab33f32e8fc595a70ebf85561df36c4
mlloyd02 <mlloyd02@gmail.com>
2017-04-14 02:53:02 -0500

    Add functionality to OptionalSnacksController

--- snacktracker/app/controllers/optional_snacks_controller.rb
+++ snacktracker/app/controllers/optional_snacks_controller.rb
@@ -1,29 +1,36 @@
 class OptionalSnacksController < ApplicationController
 
   def index
-    @this_month_sugs = Suggestion.where(:created_at => Date.today.beginning_of_month..Date.today.end_of_month)
+    this_month_sug_ids = Suggestion.where(:created_at => Date.today.beginning_of_month..1.day.from_now).pluck(:optional_snack_id)
+    @this_month_sugs = OptionalSnack.where('id' => this_month_sug_ids)
     @required_snacks = ApiService.get_required
   end
 
   def new
     @snack = OptionalSnack.new
-    @suggestion = Suggestion.new
-    @suggestions = Suggestion.where("created_at < ?", Date.today.beginning_of_month)
+    this_month_sug_ids = Suggestion.where(:created_at => Date.today.beginning_of_month..Date.today).pluck(:optional_snack_id)
+    @not_suggested_snacks = OptionalSnack.where.not('id' => this_month_sug_ids)
   end
 
   def create
-    name, loc = opt_snack_params[:name], opt_snack_params[:location]
-    ApiService.post_new_snack(name, loc)
-    ApiService.sync_db
+    if opt_snack_params[:id]
+      selected_snack = OptionalSnack.find opt_snack_params[:id]
+      selected_snack.suggestions.create
+      redirect_to '/optional_snacks'
+    else
+      name, loc = opt_snack_params[:name], opt_snack_params[:location]
+      ApiService.post_new_snack(name, loc)
+      ApiService.sync_db
 
-    sug_snack = OptionalSnack.find_by name: name
-    sug_snack.suggestions.create
-    redirect_to '/optional_snacks'
+      sug_snack = OptionalSnack.find_by name: name
+      sug_snack.suggestions.create
+      redirect_to '/optional_snacks'
+    end
   end
 
   private
   def opt_snack_params
-    params.require(:optional_snack).permit(:name, :location)
+    params.require(:optional_snack).permit(:name, :location, :id)
   end
 
 end
